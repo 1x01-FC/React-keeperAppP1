8013569018878196c67f32cd9c8bf862fda50c27
Frederic C <toonvesper@gmail.com>
2022-12-10 10:44:10 -0500

    initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <body className="note">
+            <h1>Time to chrome up!</h1>
+            <div>Choose your loadout.</div>
+        </body>
+    );
+}
+
+export default Note;
